6c0049e50b18690b6a631dac5a11b10a155a6db2
Nodar Okroshiashvili <n.okroshiashvili@gmail.com>
2023-03-30 21:54:56 +0400

    Update unit tests for Hamming

--- tests/test_hamming.rs
+++ tests/test_hamming.rs
@@ -10,26 +10,34 @@ mod tests {
 
     #[test]
     fn test_distance() {
-        assert_eq!(0.0, Hamming {src: "test".to_string(),  tar: "test".to_string()}.normalized_distance());
-        assert_eq!(1, Hamming {src: "test".to_string(),  tar: "text".to_string()}.distance());
+        assert_eq!(0, Hamming {src: "".to_string(),  tar: "".to_string()}.distance());
+        assert_eq!(0, Hamming {src: "test".to_string(),  tar: "test".to_string()}.distance());
+        assert_eq!(2, Hamming {src: "ijk".to_string(),  tar: "kji".to_string()}.distance());
+        assert_eq!(3, Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()}.distance());
     }
 
     #[test]
     fn test_normalized_distance() {
+        assert_eq!(0.0, Hamming {src: "".to_string(),  tar: "".to_string()}.normalized_distance());
         assert_eq!(0.0, Hamming {src: "test".to_string(),  tar: "test".to_string()}.normalized_distance());
-        assert_eq!(0.25, Hamming {src: "test".to_string(),  tar: "text".to_string()}.normalized_distance());
+        assert_eq!(0.6666666666666666, Hamming {src: "ijk".to_string(),  tar: "kji".to_string()}.normalized_distance());
+        assert_eq!(0.42857142857142855, Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()}.normalized_distance());
     }
 
     #[test]
     fn test_similarity() {
+        assert_eq!(0, Hamming {src: "".to_string(),  tar: "".to_string()}.similarity());
         assert_eq!(4, Hamming {src: "test".to_string(),  tar: "test".to_string()}.similarity());
-        assert_eq!(3, Hamming {src: "test".to_string(),  tar: "text".to_string()}.similarity());
+        assert_eq!(1, Hamming {src: "ijk".to_string(),  tar: "kji".to_string()}.similarity());
+        assert_eq!(4, Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()}.similarity());
     }
 
     #[test]
     fn test_normalized_similarity() {
+        assert_eq!(1.0, Hamming {src: "".to_string(),  tar: "".to_string()}.normalized_similarity());
         assert_eq!(1.0, Hamming {src: "test".to_string(),  tar: "test".to_string()}.normalized_similarity());
-        assert_eq!(0.75, Hamming {src: "test".to_string(),  tar: "text".to_string()}.normalized_similarity());
+        assert_eq!(0.33333333333333337, Hamming {src: "ijk".to_string(),  tar: "kji".to_string()}.normalized_similarity());
+        assert_eq!(0.5714285714285714, Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()}.normalized_similarity());
     }
 
     #[test]
