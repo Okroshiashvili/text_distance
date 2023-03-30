2da5ce6f4964b2add9bf4e6de1959c3960802660
Nodar Okroshiashvili <n.okroshiashvili@gmail.com>
2023-03-31 00:01:24 +0400

    Add doc tests for hamming and levenshtein

--- src/hamming.rs
+++ src/hamming.rs
@@ -1,11 +1,42 @@
 use std::cmp::max;
 
+/// Calculate the `Hamming` distance between two strings of equal length.
+/// The Hamming distance is the number of differing items in ordered sequences.
+/// [For more information see wikipedia article](https://en.wikipedia.org/wiki/Hamming_distance)
+/// 
+/// ### Examples
+/// 
+/// ```
+/// use text_distance::Hamming;
+/// 
+/// let hamming = Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+/// 
+/// assert_eq!(3, hamming.distance());
+/// assert_eq!(0.42857142857142855, hamming.normalized_distance());
+/// assert_eq!(4, hamming.similarity());
+/// assert_eq!(0.5714285714285714, hamming.normalized_similarity());
+/// 
+/// ```
+/// 
 pub struct Hamming {
     pub src: String,
     pub tar: String,
 }
 
 impl Hamming {
+    /// Calculate the `Hamming` distance between two strings of equal length.
+    /// If not equal length then panic.
+    /// 
+    /// ### Examples
+    /// 
+    /// ```
+    /// use text_distance::Hamming;
+    /// 
+    /// let hamming = Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(3, hamming.distance());
+    /// 
+    /// ```
     pub fn distance(&self) -> usize {
         if self.src.chars().count() != self.tar.chars().count() {
             panic!("Hamming distance is only defined for strings of equal length");
@@ -21,6 +52,22 @@ impl Hamming {
         return distance_counter;
     }
 
+    /// Calculate the `normalized distance` between two strings.
+    /// The normalized distance is the distance divided by the length of the longest string.
+    /// The normalized distance is always between 0.0 and 1.0.
+    /// When 0.0 then two strings are equal.
+    /// When 1.0 then two strings are completely different.
+    /// 
+    /// ### Examples
+    /// 
+    /// ```
+    /// use text_distance::Hamming;;
+    /// 
+    /// let hamming = Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(0.42857142857142855, hamming.normalized_distance());
+    /// 
+    /// ```
     pub fn normalized_distance(&self) -> f64 {
         let maximum = max(
             self.src.clone().chars().count(),
@@ -33,6 +80,22 @@ impl Hamming {
         return 0.0;
     }
 
+    /// Calculate the `similarity` between two strings.
+    /// The similarity is the length of the longest string minus the distance.
+    /// The similarity is always between 0 and the length of the longest string.
+    /// When 0 then two strings are completely different.
+    /// When the length of the longest string then two strings are equal.
+    /// 
+    /// ### Examples
+    /// 
+    /// ```
+    /// use text_distance::Hamming;
+    /// 
+    /// let hamming = Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(4, hamming.similarity());
+    /// 
+    /// ```
     pub fn similarity(&self) -> usize {
         let maximum = max(
             self.src.clone().chars().count(),
@@ -42,6 +105,23 @@ impl Hamming {
         return maximum - str_distance;
     }
 
+    /// Calculate the `normalized similarity` between two strings.
+    /// The normalized similarity is the similarity divided by the length of the longest string.
+    /// The normalized similarity is always between 0.0 and 1.0.
+    /// When 0.0 then two strings are completely different.
+    /// When 1.0 then two strings are equal.
+    /// The normalized similarity is the same as 1.0 minus the normalized distance.
+    /// 
+    /// ### Examples
+    /// 
+    /// ```
+    /// use text_distance::Hamming;
+    /// 
+    /// let hamming = Hamming {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(0.5714285714285714, hamming.normalized_similarity());
+    /// 
+    /// ```
     pub fn normalized_similarity(&self) -> f64 {
         let str_normalized_distance = self.normalized_distance();
         return 1.0 - str_normalized_distance;
--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -1,12 +1,47 @@
 use std::cmp::max;
 
+/// Calculate the `Levenshtein` distance between two strings.
+/// The Levenshtein distance between two words is the minimum number of single-character edits
+/// (insertions, deletions or substitutions) required to change one word into the other.
+/// [For more information see wikipedia article](https://en.wikipedia.org/wiki/Levenshtein_distance)
+/// 
+/// ### Examples
+/// 
+/// ```
+/// use text_distance::Levenshtein;
+/// 
+/// let levenshtein = Levenshtein {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+/// 
+/// assert_eq!(3, levenshtein.distance());
+/// assert_eq!(0.42857142857142855, levenshtein.normalized_distance());
+/// assert_eq!(4, levenshtein.similarity());
+/// assert_eq!(0.5714285714285714, levenshtein.normalized_similarity());
+/// 
+/// ```
+/// 
 pub struct Levenshtein {
     pub src: String,
     pub tar: String,
 }
 
 impl Levenshtein {
-    /// Calculate the Levenshtein distance between two strings.
+    /// Calculate the `Levenshtein` distance between two strings.
+    /// The distance is the number of edit operations needed to transform the source string into the target string.
+    /// The edit operations are:
+    /// - deletion (removal) of a single character from any position in the string.
+    /// - insertion (addition) of a single character at any position in the string.
+    /// - substitution (replacement) of a single character with another character.
+    /// 
+    /// ### Examples
+    /// 
+    /// ```
+    /// use text_distance::Levenshtein;
+    /// 
+    /// let levenshtein = Levenshtein {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(3, levenshtein.distance());
+    /// 
+    /// ```
     pub fn distance(&self) -> usize {
         let len_src = self.src.chars().count();
         let len_tar = self.tar.chars().count();
@@ -37,6 +72,21 @@ impl Levenshtein {
         return matrix[len_src][len_tar];
     }
 
+    /// Calculate the `normalized distance` between two strings.
+    /// The normalized distance is the distance divided by the length of the longest string.
+    /// The normalized distance is always between 0.0 and 1.0.
+    /// When 0.0 then two strings are equal.
+    /// When 1.0 then two strings are completely different.
+    /// 
+    /// ## Examples
+    /// ```
+    /// use text_distance::Levenshtein;
+    /// 
+    /// let levenshtein = Levenshtein {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(0.42857142857142855, levenshtein.normalized_distance());
+    /// 
+    /// ```
     pub fn normalized_distance(&self) -> f64 {
         let maximum = max(
             self.src.clone().chars().count(),
@@ -49,6 +99,22 @@ impl Levenshtein {
         return 0.0;
     }
 
+    /// Calculate the `similarity` between two strings.
+    /// The similarity is the length of the longest string minus the distance.
+    /// The similarity is always between 0 and the length of the longest string.
+    /// When 0 then two strings are completely different.
+    /// When the length of the longest string then two strings are equal.
+    /// 
+    /// ## Examples
+    /// 
+    /// ```
+    /// use text_distance::Levenshtein;
+    /// 
+    /// let levenshtein = Levenshtein {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(4, levenshtein.similarity());
+    /// 
+    /// ```
     pub fn similarity(&self) -> usize {
         let maximum = max(
             self.src.clone().chars().count(),
@@ -58,6 +124,23 @@ impl Levenshtein {
         return maximum - str_distance;
     }
 
+    /// Calculate the `normalized similarity` between two strings.
+    /// The normalized similarity is the similarity divided by the length of the longest string.
+    /// The normalized similarity is always between 0.0 and 1.0.
+    /// When 0.0 then two strings are completely different.
+    /// When 1.0 then two strings are equal.
+    /// The normalized similarity is the same as 1.0 minus the normalized distance.
+    /// 
+    /// ## Examples
+    /// 
+    /// ```
+    /// use text_distance::Levenshtein;
+    /// 
+    /// let levenshtein = Levenshtein {src: "karolin".to_string(),  tar: "kathrin".to_string()};
+    /// 
+    /// assert_eq!(0.5714285714285714, levenshtein.normalized_similarity());
+    /// 
+    /// ```
     pub fn normalized_similarity(&self) -> f64 {
         let str_normalized_distance = self.normalized_distance();
         return 1.0 - str_normalized_distance;
