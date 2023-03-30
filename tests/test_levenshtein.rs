d8ce08878dfccfee4507f3e7976feb9ff96ea888
Nodar Okroshiashvili <n.okroshiashvili@gmail.com>
2023-03-30 21:43:12 +0400

    Update unit tests for Levenshtein

--- tests/test_levenshtein.rs
+++ tests/test_levenshtein.rs
@@ -10,45 +10,37 @@ mod tests {
 
     #[test]
     fn test_distance() {
-        assert_eq!(1, Levenshtein {src: "test".to_string(),  tar: "text".to_string()}.distance());
-        assert_eq!(2, Levenshtein {src: "book".to_string(),  tar: "back".to_string()}.distance());
-        assert_eq!(5, Levenshtein {src: "person".to_string(),  tar: "human".to_string()}.distance());
-
-        assert_eq!(0, Levenshtein {src: "test".to_string(),  tar: "test".to_string()}.distance());
-        assert_eq!(0, Levenshtein {src: "t".to_string(),  tar: "t".to_string()}.distance());
-
-        assert_eq!(1, Levenshtein {src: "Test".to_string(),  tar: "Text".to_string()}.distance());
-        assert_eq!(2, Levenshtein {src: "test".to_string(),  tar: "Text".to_string()}.distance()); // distnace is 2, should be 1
-
-        assert_eq!(4, Levenshtein {src: "test".to_string(),  tar: "".to_string()}.distance());
-        assert_eq!(4, Levenshtein {src: "".to_string(),  tar: "text".to_string()}.distance());
         assert_eq!(0, Levenshtein {src: "".to_string(),  tar: "".to_string()}.distance());
+        assert_eq!(4, Levenshtein {src: "test".to_string(),  tar: "".to_string()}.distance());
+        assert_eq!(5, Levenshtein {src: "abcdef".to_string(),  tar: "axb123".to_string()}.distance());
+        assert_eq!(6, Levenshtein {src: "levenshtein".to_string(),  tar: "frankenstein".to_string()}.distance());
+        assert_eq!(15, Levenshtein {src: "python is an interpreted language".to_string(),  tar: "rust is a compiled language".to_string()}.distance());
     }
 
     #[test]
     fn test_normalized_distance() {
-        assert_eq!(0.0, Levenshtein {src: "test".to_string(),  tar: "test".to_string()}.normalized_distance());
-        assert_eq!(0.25, Levenshtein {src: "test".to_string(),  tar: "text".to_string()}.normalized_distance());
-        assert_eq!(0.5, Levenshtein {src: "book".to_string(),  tar: "back".to_string()}.normalized_distance());
-        assert_eq!(0.8333333333333334, Levenshtein {src: "person".to_string(),  tar: "human".to_string()}.normalized_distance());
-        assert_eq!(1.0, Levenshtein {src: "test".to_string(),  tar: "book".to_string()}.normalized_distance());
+        assert_eq!(0.0, Levenshtein {src: "".to_string(),  tar: "".to_string()}.normalized_distance());
+        assert_eq!(1.0, Levenshtein {src: "test".to_string(),  tar: "".to_string()}.normalized_distance());
+        assert_eq!(0.8333333333333334, Levenshtein {src: "abcdef".to_string(),  tar: "axb123".to_string()}.normalized_distance());
+        assert_eq!(0.5, Levenshtein {src: "levenshtein".to_string(),  tar: "frankenstein".to_string()}.normalized_distance());
+        assert_eq!(0.45454545454545453, Levenshtein {src: "python is an interpreted language".to_string(),  tar: "rust is a compiled language".to_string()}.normalized_distance());
     }
 
     #[test]
     fn test_similarity() {
-        assert_eq!(4, Levenshtein {src: "test".to_string(),  tar: "test".to_string()}.similarity());
-        assert_eq!(3, Levenshtein {src: "test".to_string(),  tar: "text".to_string()}.similarity());
-        assert_eq!(2, Levenshtein {src: "book".to_string(),  tar: "back".to_string()}.similarity());
-        assert_eq!(1, Levenshtein {src: "person".to_string(),  tar: "human".to_string()}.similarity());
-        assert_eq!(0, Levenshtein {src: "test".to_string(),  tar: "book".to_string()}.similarity());
+        assert_eq!(0, Levenshtein {src: "".to_string(),  tar: "".to_string()}.similarity());
+        assert_eq!(0, Levenshtein {src: "test".to_string(),  tar: "".to_string()}.similarity());
+        assert_eq!(1, Levenshtein {src: "abcdef".to_string(),  tar: "axb123".to_string()}.similarity());
+        assert_eq!(6, Levenshtein {src: "levenshtein".to_string(),  tar: "frankenstein".to_string()}.similarity());
+        assert_eq!(18, Levenshtein {src: "python is an interpreted language".to_string(),  tar: "rust is a compiled language".to_string()}.similarity());
     }
 
     #[test]
     fn test_normalized_similarity() {
-        assert_eq!(1.0, Levenshtein {src: "test".to_string(),  tar: "test".to_string()}.normalized_similarity());
-        assert_eq!(0.75, Levenshtein {src: "test".to_string(),  tar: "text".to_string()}.normalized_similarity());
-        assert_eq!(0.5, Levenshtein {src: "book".to_string(),  tar: "back".to_string()}.normalized_similarity());
-        assert_eq!(0.16666666666666663, Levenshtein {src: "person".to_string(),  tar: "human".to_string()}.normalized_similarity());
-        assert_eq!(0.0, Levenshtein {src: "test".to_string(),  tar: "book".to_string()}.normalized_similarity());
+        assert_eq!(1.0, Levenshtein {src: "".to_string(),  tar: "".to_string()}.normalized_similarity());
+        assert_eq!(0.0, Levenshtein {src: "test".to_string(),  tar: "".to_string()}.normalized_similarity());
+        assert_eq!(0.16666666666666663, Levenshtein {src: "abcdef".to_string(),  tar: "axb123".to_string()}.normalized_similarity());
+        assert_eq!(0.5, Levenshtein {src: "levenshtein".to_string(),  tar: "frankenstein".to_string()}.normalized_similarity());
+        assert_eq!(0.5454545454545454, Levenshtein {src: "python is an interpreted language".to_string(),  tar: "rust is a compiled language".to_string()}.normalized_similarity());
     }
 }
