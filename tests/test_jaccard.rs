f425e7ee1e8b2454b667b4fbcb099b11c60109fe
Nodar Okroshiashvili <n.okroshiashvili@gmail.com>
2023-04-06 18:50:12 +0400

    Add Jaccard distance support

---
+++ tests/test_jaccard.rs
@@ -0,0 +1,48 @@
+
+// TODO: Add approximate float comparison
+// https://docs.rs/float-cmp/latest/float_cmp/
+// https://docs.rs/approx/latest/approx/
+
+
+#[cfg(test)]
+mod tests {
+    use text_distance::Jaccard;
+
+    #[test]
+    fn test_distance() {
+        assert_eq!(0.5555556, Jaccard {src: "data is the new oil of the digital economy".to_string(),  tar: "data is a new oil".to_string(), qval: 0}.distance());
+        assert_eq!(0.3333333, Jaccard {src: "nelson".to_string(),  tar: "neilsen".to_string(), qval: 1}.distance());
+        assert_eq!(0.45, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 2}.distance());
+        assert_eq!(0.61904764, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 3}.distance());
+    }
+
+    #[test]
+    fn test_normalized_distance() {
+        assert_eq!(0.5555556, Jaccard {src: "data is the new oil of the digital economy".to_string(),  tar: "data is a new oil".to_string(), qval: 0}.normalized_distance());
+        assert_eq!(0.3333333, Jaccard {src: "nelson".to_string(),  tar: "neilsen".to_string(), qval: 1}.normalized_distance());
+        assert_eq!(0.45, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 2}.normalized_distance());
+        assert_eq!(0.61904764, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 3}.normalized_distance());
+    }
+
+    #[test]
+    fn test_similarity() {
+        assert_eq!(0.44444442, Jaccard {src: "data is the new oil of the digital economy".to_string(),  tar: "data is a new oil".to_string(), qval: 0}.similarity());
+        assert_eq!(0.6666667, Jaccard {src: "nelson".to_string(),  tar: "neilsen".to_string(), qval: 1}.similarity());
+        assert_eq!(0.55, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 2}.similarity());
+        assert_eq!(0.38095236, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 3}.similarity());
+    }
+
+    #[test]
+    fn test_normalized_similarity() {
+        assert_eq!(0.44444442, Jaccard {src: "data is the new oil of the digital economy".to_string(),  tar: "data is a new oil".to_string(), qval: 0}.normalized_similarity());
+        assert_eq!(0.6666667, Jaccard {src: "nelson".to_string(),  tar: "neilsen".to_string(), qval: 1}.normalized_similarity());
+        assert_eq!(0.55, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 2}.normalized_similarity());
+        assert_eq!(0.38095236, Jaccard {src: "nelson has a car".to_string(),  tar: "neilsen has a cat".to_string(), qval: 3}.normalized_similarity());
+    }
+
+    #[test]
+    #[should_panic(expected = "Can't create n-grams from text shorter than n-gram length")]
+    fn test_jaccard_panic() {
+        Jaccard {src: "nelson".to_string(),  tar: "neilsen".to_string(), qval: 100}.distance();
+    }
+}
